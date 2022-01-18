53b15e85b6c3a235b52aaa35089835f8eff35699
Michael Feil <michaelfeil@example.com>
2022-01-18 16:36:03 +0100

    final changes on git

--- s8_deployment/exercise_files/function/main.py
+++ s8_deployment/exercise_files/function/main.py
@@ -1,22 +1,33 @@
 from google.cloud import storage
 import pickle
+import sklearn
 
-BUCKET_NAME = "gs/cloud_fct_michaelf"
-MODEL_FILE = "sklearn_model/model.pkl"
+BUCKET_NAME = "cloud_fct_michaelf"
+MODEL_FILE = "model.pkl"
 
 client = storage.Client()
 bucket = client.get_bucket(BUCKET_NAME)
 blob = bucket.get_blob(MODEL_FILE)
 my_model = pickle.loads(blob.download_as_string())
 
-def knn_classifier(request):
-   """ will to stuff to your request """
-   request_json = request.get_json()
-   if request_json and 'input_data' in request_json:
-         data = request_json['input_data']
-         input_data = list(map(int, data.split(',')))
-         prediction = my_model.predict([input_data])
-         return f'Belongs to class: {prediction}'
-   else:
-         return 'No input data received'
+def handle_request(request):
+    """handle """
+    request_json = request.get_json()
+    return knn_classifier(request_json)
+    
+    
+def knn_classifier(request_json):
+    """ will to stuff to your request """
+    if request_json and 'input_data' in request_json:
+        data = request_json['input_data']
+        input_data = list(map(int, data.split(',')))
+        prediction = my_model.predict([input_data])
+        return f'Belongs to class: {prediction}'
+    else:
+        return 'No input data received'
 
+def test_function():
+    print(knn_classifier({"input_data": "1,1,1,1"}))
+
+if __name__ == "__main__":
+    test_function()
--- s9_monitoring/exercise_files/alert.py
+++ s9_monitoring/exercise_files/alert.py
@@ -1,8 +1,9 @@
 import time
 import requests
-url = 'https://us-central1-dtumlops-335110.cloudfunctions.net/function-2'
-payload = {'message': 'Hello, General Kenobi'}
+url = 'https://us-central1-silver-asset-337912.cloudfunctions.net/function-test'
+payload = {'message': 'Hello, General Yoda'}
 
-for _ in range(1000):
+for _ in range(10):
    r = requests.get(url, params=payload)
+   print(r.content)
 
